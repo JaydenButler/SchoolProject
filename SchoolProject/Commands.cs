31fec413060524507a849b729bc2a368b2426e86
Jayden <jaydenbutler4457@gmail.com>
2020-08-01 20:57:23 +1000

    Version 1.0.7 - Fixed DM's not working

--- SchoolProject/Commands.cs
+++ SchoolProject/Commands.cs
@@ -104,8 +104,7 @@ namespace SchoolProject
         public async Task ReadOrgAsync (string org)
         {
             var user = Context.User as SocketGuildUser;
-            var staffRole = user.Roles.FirstOrDefault (x => x.Name == "Staff");
-            if (Context.IsPrivate == true || Context.Channel.Name == "bot-commands" || staffRole != null)
+            if (Context.IsPrivate == true)
             {
                 var rec = MongoCRUD.Instance.LoadRecordById<OrgModel> (org, "OrgDatabase", "OrgName");
                 EmbedBuilder builder = new EmbedBuilder ();
@@ -119,25 +118,62 @@ namespace SchoolProject
 
                 await ReplyAsync ("", false, builder.Build ());
             }
+            else
+            {
+                var staffRole = user.Roles.FirstOrDefault (x => x.Name == "Staff");
+                if (Context.Channel.Name == "bot-commands" || staffRole != null)
+                {
+                    var rec = MongoCRUD.Instance.LoadRecordById<OrgModel> (org, "OrgDatabase", "OrgName");
+                    EmbedBuilder builder = new EmbedBuilder ();
+                    builder.WithTitle ($"**{rec.OrgName}**").WithDescription ($"Twitter Link: {rec.socialModel.TwitterLink}\nFacebook Link: {rec.socialModel.FacebookLink}\n" +
+                        $"Instagram Link: {rec.socialModel.InstagramLink}\nYoutube Channel: {rec.socialModel.YoutubeLinks}\nTwitch Team: {rec.socialModel.TwitchTeam}\nWebsite Link: {rec.WebsiteLink}\nLogo Link: {rec.LogoLink}").WithColor (Discord.Color.Red);
+
+                    if (rec.LogoLink != "N/A")
+                    {
+                        builder.WithThumbnailUrl (rec.LogoLink);
+                    }
+
+                    await ReplyAsync ("", false, builder.Build ());
+                }
+            }
         }
+
         [Command ("get")]
         public async Task ReadOrgAsync (string org1, string org2)
         {
             string org = org1 + " " + org2;
             var user = Context.User as SocketGuildUser;
-            var staffRole = user.Roles.FirstOrDefault (x => x.Name == "Staff");
-            if (Context.IsPrivate == true || Context.Channel.Name == "bot-commands" || staffRole != null)
+            if (Context.IsPrivate == true)
             {
                 var rec = MongoCRUD.Instance.LoadRecordById<OrgModel> (org, "OrgDatabase", "OrgName");
                 EmbedBuilder builder = new EmbedBuilder ();
                 builder.WithTitle ($"**{rec.OrgName}**").WithDescription ($"Twitter Link: {rec.socialModel.TwitterLink}\nFacebook Link: {rec.socialModel.FacebookLink}\n" +
                     $"Instagram Link: {rec.socialModel.InstagramLink}\nYoutube Channel: {rec.socialModel.YoutubeLinks}\nTwitch Team: {rec.socialModel.TwitchTeam}\nWebsite Link: {rec.WebsiteLink}\nLogo Link: {rec.LogoLink}").WithColor (Discord.Color.Red);
+
                 if (rec.LogoLink != "N/A")
                 {
                     builder.WithThumbnailUrl (rec.LogoLink);
                 }
 
                 await ReplyAsync ("", false, builder.Build ());
+            }
+            else
+            {
+                var staffRole = user.Roles.FirstOrDefault (x => x.Name == "Staff");
+                if (Context.Channel.Name == "bot-commands" || staffRole != null)
+                {
+                    var rec = MongoCRUD.Instance.LoadRecordById<OrgModel> (org, "OrgDatabase", "OrgName");
+                    EmbedBuilder builder = new EmbedBuilder ();
+                    builder.WithTitle ($"**{rec.OrgName}**").WithDescription ($"Twitter Link: {rec.socialModel.TwitterLink}\nFacebook Link: {rec.socialModel.FacebookLink}\n" +
+                        $"Instagram Link: {rec.socialModel.InstagramLink}\nYoutube Channel: {rec.socialModel.YoutubeLinks}\nTwitch Team: {rec.socialModel.TwitchTeam}\nWebsite Link: {rec.WebsiteLink}\nLogo Link: {rec.LogoLink}").WithColor (Discord.Color.Red);
+
+                    if (rec.LogoLink != "N/A")
+                    {
+                        builder.WithThumbnailUrl (rec.LogoLink);
+                    }
+
+                    await ReplyAsync ("", false, builder.Build ());
+                }
             }
         }
 
